5d945f2f6feead69c476cc1616531ac1b07bfb40
Camila Matos <camilamatos7186@gmail.com>
2023-09-24 12:30:39 -0300

    Desenvolvido funções do "Estoque"

--- Estoque/Classes/Estoque.php
+++ Estoque/Classes/Estoque.php
@@ -1,4 +1,5 @@
 <?php
+require_once "Conecta.php";
 class Estoque {
     private $idEstoque;
     private $nome;
@@ -49,20 +50,90 @@ class Estoque {
         return $this;
     }
 
-    public function cadastrarEstoque($nome, $descricao, $idStatus){
-        
+    public function cadastrarEstoque(){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "insert into estoque values (NULL, :nome, :descricao, :idStatus)";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":nome", $this->nome);
+        $consulta->bindParam(":descricao", $this->descricao);
+        $consulta->bindParam(":idStatus", $this->idStatus);
+
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
     }
 
-    public function editarEstoque($idEstoque, $nome, $descricao){
+    public function editarEstoque($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "update estoque SET nome=:nome, descricao=:descricao where idEstoque=:idEstoque";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":nome", $this->nome);
+        $consulta->bindParam(":descricao", $this->descricao);
+        $consulta->bindParam(":idEstoque", $id);
 
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
     }
 
-    public function excluirEstoque($idEstoque){
+    public function excluirEstoque($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        //verificar se não há lotes cadastrados com esse estoque
+        if(empty($this->verificarRegistros($id))){
+            $sql = "delete from estoque where idEstoque=:idEstoque";
+            $consulta = $pdo->prepare($sql);
+            $consulta->bindParam(":idEstoque", $id);
 
+            if ($consulta->execute()) {
+                $resultado = "S";//sucesso
+            } else {
+                $resultado = "E";//erro
+            }
+        } else {
+            $resultado = "R";//operação recusada, não é permitido excluir categorias com item registrados
+        }
+
+        return $resultado;
     }
 
-    public function alterarStatusEstoque($idEstoque, $idStatus){
+    public function alterarStatusEstoque($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "update estoque SET idStatus=:idStatus where idEstoque=:idEstoque";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idStatus", $this->idStatus);
+        $consulta->bindParam(":idEstoqueStatus", $id);
 
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
+    }
+
+    public function verificarRegistros($id) {
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "select idEstoque from lote where idEstoque=:idEstoque";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idEstoque", $id);
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_OBJ);
+
+        return $resultado;
     }
 
 }
