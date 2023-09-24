d6d4f8d6cb329c26cbf89d8f4e52bd47873b1b0c
Camila Matos <camilamatos7186@gmail.com>
2023-09-24 14:56:08 -0300

    Desenvolvido funções de "UnidadeMedida"

--- Estoque/Classes/UnidadeMedida.php
+++ Estoque/Classes/UnidadeMedida.php
@@ -1,4 +1,5 @@
 <?php
+require_once "Conecta.php";
 class UnidadeMedida {
     private $idUnidadeMedida;
     private $nome;
@@ -33,16 +34,70 @@ class UnidadeMedida {
         return $this;
     }
 
-    public function cadastrarUnidadeMedida($nome){
-        
+    public function cadastrarUnidadeMedida(){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "insert into unidadeMedida values (NULL, :nome)";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":nome", $this->nome);
+
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
     }
 
-    public function editarUnidadeMedida($idUnidadeMedida, $nome){
+    public function editarUnidadeMedida($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "update unidadeMedida SET nome=:nome where idUnidadeMedida=:idUnidadeMedida";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":nome", $this->nome);
+        $consulta->bindParam(":idUnidadeMedida", $id);
 
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
     }
 
-    public function excluirUnidadeMedida($idUnidadeMedida){
+    public function excluirUnidadeMedida($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        //verificar se não há itens cadastrados com essa unidade de medida
+        if(empty($this->verificarRegistros($id))){
+            $sql = "delete from unidadeMedida where idUnidadeMedida=:idUnidadeMedida";
+            $consulta = $pdo->prepare($sql);
+            $consulta->bindParam(":idUnidadeMedida", $id);
 
+            if ($consulta->execute()) {
+                $resultado = "S";//sucesso
+            } else {
+                $resultado = "E";//erro
+            }
+        } else {
+            $resultado = "R";//operação recusada
+        }
+
+        return $resultado;
+    }
+
+    public function verificarRegistros($id) {
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "select * from item where idUnidadeMedida=:idUnidadeMedida";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idUnidadeMedida", $id);
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_OBJ);
+
+        return $resultado;
     }
 
 }
