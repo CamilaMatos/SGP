0c8f72f07d1e34b2ae621ea40bc541524cd61c13
Camila Matos <camilamatos7186@gmail.com>
2023-09-24 12:30:17 -0300

    Pagina para teste das funções

---
+++ Estoque/Pages/teste.php
@@ -0,0 +1,9 @@
+<?php
+include "../Classes/Estoque.php";
+
+// $C= new Categoria(null, "Bebidas");
+// // $C->setIdCentroCusto(2);
+// // $C->setNome("Teste 3");
+// print($C->cadastrarCategoria());
+
+$C= new Estoque(1, "Cozinhas", "Coizinha central e de Finalização", 1);
--- Estoque/Pages/texte.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "../Classes/Categoria.php";
-$teste = "Text";
-$C= new Categoria("Limpeza");
-$C->cadastrarCategoria("Alimento");
-
