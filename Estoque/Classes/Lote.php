4cc63bf9e382098b8e4a19ead4e7f26834b29304
Camila Matos <camilamatos7186@gmail.com>
2023-09-24 13:31:19 -0300

    Desenvolvido funções de "Lote"

--- Estoque/Classes/Lote.php
+++ Estoque/Classes/Lote.php
@@ -1,4 +1,5 @@
 <?php
+require_once "Conecta.php";
 class Lote {
     private $idLote;
     private $idItem;
@@ -103,15 +104,93 @@ class Lote {
         return $this;
     }
 
-    public function inserirLote($idItem, $idEstoque, $quantidadeInicial, $quantidadeAtual, $validade, $valorUnitario){
-        
+    public function inserirLote(){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "insert into lote values (NULL, :idItem, :idEstoque, :quantidadeInicial, :quantidadeAtual, :validade, :valorUnitario)";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idItem", $this->idItem);
+        $consulta->bindParam(":idEstoque", $this->idEstoque);
+        $consulta->bindParam(":quantidadeInicial", $this->quantidadeInicial);
+        $consulta->bindParam(":quantidadeAtual", $this->quantidadeAtual);
+        $consulta->bindParam(":validade", $this->validade);
+        $consulta->bindParam(":valorUnitario", $this->valorUnitario);
+
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
     }
 
-    public function editarLote($idItem, $idEstoque, $quantidadeInicial, $quantidadeAtual, $validade, $valorUnitario){
+    public function editarLote($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "update lote SET idEstoque=:idEstoque, quantidadeInicial=:quantidadeInicial, quantidadeAtual=:quantidadeAtual, validade=:validade, valorUnitario=:valorUnitario where idLote=:idLote";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idItem", $this->idItem);
+        $consulta->bindParam(":idEstoque", $this->idEstoque);
+        $consulta->bindParam(":quantidadeInicial", $this->quantidadeInicial);
+        $consulta->bindParam(":quantidadeAtual", $this->quantidadeAtual);
+        $consulta->bindParam(":validade", $this->validade);
+        $consulta->bindParam(":valorUnitario", $this->valorUnitario);
+        $consulta->bindParam(":idLote", $id);
 
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
     }
+
+    public function excluirLote($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        //verificar se a solicitação ainda não foi atendida
+        if(empty($this->verificarRegistros($id))){
+            $sql = "delete from lote where idLote=:idLote";
+            $consulta = $pdo->prepare($sql);
+            $consulta->bindParam(":idLote", $id);
+
+            if ($consulta->execute()) {
+                $resultado = "S";//sucesso
+            } else {
+                $resultado = "E";//erro
+            }
+        } else {
+            $resultado = "R";//operação recusada, não é permitido excluir categorias com item registrados
+        }
+
+        return $resultado;
+    }
+
+    public function verificarRegistros($id) {
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "select * from itensSolicitacao where idLote=:idLote";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idLote", $id);
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_OBJ);
+
+        return $resultado;
+    }
+
 
     public function consultarEstoqueItem($idItem, $idEstoque){
-        
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "select SUM(quantidadeAtual) from lote where idItem=:idItem and idEstoque=:idEstoque";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idItem", $idItem);
+        $consulta->bindParam(":idEstoque", $idEstoque);
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_OBJ);
+
+        return $resultado;
     }
 }
