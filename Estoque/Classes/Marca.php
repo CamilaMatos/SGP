0a02f07cc2da00c7437358a3510c21b018844111
Camila Matos <camilamatos7186@gmail.com>
2023-09-24 13:38:34 -0300

    Desenvolvido funções de "Marca"

--- Estoque/Classes/Marca.php
+++ Estoque/Classes/Marca.php
@@ -1,4 +1,5 @@
 <?php
+require_once "Conecta.php";
 class Marca {
     private $idMarca;
     private $nome;
@@ -33,16 +34,70 @@ class Marca {
         return $this;
     }
 
-    public function cadastrarMarca($nome){
-        
+    public function cadastrarMarca(){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "insert into marca values (NULL, :nome)";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":nome", $this->nome);
+
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
     }
 
-    public function editarMarca($idMarca, $nome){
+    public function editarMarca($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "update marca SET nome=:nome where idMarca=:idMarca";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":nome", $this->nome);
+        $consulta->bindParam(":idMarca", $id);
 
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
     }
 
-    public function excluirMarca($idMarca){
+    public function excluirMarca($id){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        //verificar se não há itens cadastrados com essa marca
+        if(empty($this->verificarRegistros($id))){
+            $sql = "delete from marca where idMarca=:idMarca";
+            $consulta = $pdo->prepare($sql);
+            $consulta->bindParam(":idMarca", $id);
 
+            if ($consulta->execute()) {
+                $resultado = "S";//sucesso
+            } else {
+                $resultado = "E";//erro
+            }
+        } else {
+            $resultado = "R";//operação recusada
+        }
+
+        return $resultado;
+    }
+
+    public function verificarRegistros($id) {
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "select * from item where idMarca=:idMarca";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idMarca", $id);
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_OBJ);
+
+        return $resultado;
     }
 
 }
