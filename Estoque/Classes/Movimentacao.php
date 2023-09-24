313e8ce26a711fa6398aca4c4f1bb4413b3b18c2
Camila Matos <camilamatos7186@gmail.com>
2023-09-24 14:27:26 -0300

    Desenvolvido as funções de "Movimentação"
    Desenvolvido apenas funções básicas de movimentação, vamos entender quais outras funções serão necessárias nessa classe

--- Estoque/Classes/Movimentacao.php
+++ Estoque/Classes/Movimentacao.php
@@ -1,4 +1,6 @@
 <?php
+require_once "Conecta.php";
+require_once "Consultar.php";
 class Movimentacao {
     private $idMovimentacao;
     private $idSolicitacao;
@@ -75,7 +77,56 @@ class Movimentacao {
         return $this;
     }
 
-    public function realizarMovimentacao($idSolicitacao, $idUsuario, $idStatus, $data){
-        
+    public function registrarMovimentacao($idSolicitacao, $idUsuario, $idStatus, $data){
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "insert into movimentacao values (NULL, :idSolicitacao, :idUsuario, :idStatus, :data)";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idSolicitacao", $this->idSolicitacao);
+        $consulta->bindParam(":idUsuario", $this->idUsuario);
+        $consulta->bindParam(":idStatus", $this->idStatus);
+        $consulta->bindParam(":data", $this->data);
+
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
+    }
+
+    public function baixarItem($idLote) {
+        $qtdSolicitada = $this->verificarQuantidade($idLote);
+        $consulta = new Consultar($idLote, NULL);
+        $qtdLote = $consulta->quantidadeLote();
+        $quantidade = $qtdLote->quantidadeAtual - $qtdSolicitada->quantidade;
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "update lote SET quantidadeAtual=:quantidadeAtual where idLote=:idLote";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":quantidadeAtual", $quantidade);
+        $consulta->bindParam(":idLote", $idLote);
+
+        if ($consulta->execute()) {
+            $resultado = "S";//sucesso
+        } else {
+            $resultado = "E";//erro
+        }
+
+        return $resultado;
+    }
+
+    public function verificarQuantidade($id) {
+        $conectar = new Conecta();
+        $pdo = $conectar->conectar();
+        $sql = "select quantidade from itensSolicitacao where idSolicitacao=:idSolicitacao and idLote=:idLote";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":idSolicitacao", $this->idSolicitacao);
+        $consulta->bindParam(":idLote", $id);
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_OBJ);
+
+        return $resultado;
     }
 }
